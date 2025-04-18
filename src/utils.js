d03a8bba919add3f2fa8dd79d1e39c6a7053596e
DiditGan <162329879+DiditGan@users.noreply.github.com>
2025-04-19 03:12:23 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://console.cloud.google.com/cloud-build/builds;region=us-central1/6149bb40-95b7-45f8-b24a-c524858e178a?hl=en&project=c-04-451109";
+export const BASE_URL = "https://tcc-be-yedhit-285761334624.us-central1.run.app";
